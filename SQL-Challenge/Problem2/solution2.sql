9c7ae7628d89ddc50cd409ff7bd6364ba010ca1d
Maniyajeswani <maniyajeswani221142@acropolis.in>
2026-02-16 18:55:06 +0530

    Complete Assessment solutions - Solved 5 SQL challenges and 1 python challenge

--- SQL-Challenge/Problem2/solution2.sql
+++ SQL-Challenge/Problem2/solution2.sql
@@ -1,1 +1,58 @@
 -- write your solution here
+WITH cte AS (
+    SELECT * FROM salary
+    CROSS JOIN (
+        SELECT *, 'income' as type FROM income
+        UNION ALL
+        SELECT *, 'deduction' as type FROM deduction
+    ) t
+)
+
+SELECT 
+    emp_id,
+    emp_name,
+    base_salary,
+
+    MAX(CASE WHEN income = 'Basic' 
+             THEN ROUND((percentage * base_salary)/100,0) END) AS Basic,
+
+    MAX(CASE WHEN income = 'Allowance' 
+             THEN ROUND((percentage * base_salary)/100,0) END) AS Allowance,
+
+    MAX(CASE WHEN income = 'Others' 
+             THEN ROUND((percentage * base_salary)/100,0) END) AS Others,
+
+    MAX(CASE WHEN income = 'Insurance' 
+             THEN ROUND((percentage * base_salary)/100,0) END) AS Insurance,
+
+    MAX(CASE WHEN income = 'Health' 
+             THEN ROUND((percentage * base_salary)/100,0) END) AS Health,
+
+    MAX(CASE WHEN income = 'House' 
+             THEN ROUND((percentage * base_salary)/100,0) END) AS House,
+
+        SUM(CASE 
+            WHEN type = 'income'
+            THEN ROUND((percentage * base_salary)/100,0)
+            ELSE 0
+        END) AS GROSS,
+    SUM(CASE 
+            WHEN type = 'deduction'
+            THEN ROUND((percentage * base_salary)/100,0)
+            ELSE 0
+        END) AS TOTAL_DEDUCTIONS,
+        
+        SUM(CASE 
+        WHEN type = 'income'
+        THEN ROUND((percentage * base_salary)/100,0)
+        ELSE 0
+    END)
+-
+SUM(CASE 
+        WHEN type = 'deduction'
+        THEN ROUND((percentage * base_salary)/100,0)
+        ELSE 0
+    END) AS net_pay
+FROM cte
+GROUP BY emp_id, emp_name, base_salary
+ORDER BY emp_id;
